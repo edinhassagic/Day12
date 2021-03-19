96a7e2056720d523247ff93bbb678e42c806a77f
edin hasagic <edinhassagic@gmail.com>
2021-03-19 22:55:29 +0100

    5

---
+++ ex05/src/index.js
@@ -0,0 +1,24 @@
+var myPetsArray = [
+  {
+    animalType: [
+        "Dog", 
+        "Cat", 
+        "Bird" ]
+  },
+  
+  {
+    name: [
+        "Pujdo", 
+        "Maca", 
+        "Tweety" ]
+  },
+];
+
+function myPetsFunction(pets) {
+return  myPetsArray[1].name[1];
+
+}
+
+console.log(myPetsFunction(myPetsArray));
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
